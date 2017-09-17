a0d52a979a1dd91401c673b1104930a69fe85552
jitensoni24 <jitendrasoni24@gmail.com>
2017-09-17 21:38:59 +0100

    removed ch 8 and added ch 9, 10

---
+++ ch-10/function-2/excercise-2.js
@@ -0,0 +1,17 @@
+var num = -10;
+for(var i = -1; i <= 9; i++) {
+	console.log(i);
+}
+
+var eve = 10;
+for(var i = 10; i < 20; i++) {
+	if(i % 2 === 0) {
+		console.log(i);
+	}
+}
+
+
+var odd = 300;
+
+
+var findDiv = 5;
